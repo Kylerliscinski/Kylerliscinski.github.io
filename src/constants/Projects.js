d8745c1d33d8fc74a57a70b37824b206ab37ce44
Kylerliscinski <62256404+Kylerliscinski@users.noreply.github.com>
2024-06-11 11:39:26 -0600

    Added images for projects

--- src/constants/Projects.js
+++ src/constants/Projects.js
@@ -1,4 +1,4 @@
-import { cwChallenges, grantReady, threaditAI, gamePursuit } from '../assets/img/projects/_index';
+import { cwChallenges, grantReady, threaditAI, gamePursuit, spuditions, keepr, inspire } from '../assets/img/projects/_index';
 import { github } from '../assets/img/icons/_index';
 
 const projectData = {
@@ -11,7 +11,7 @@ const projectData = {
       id: "keepr",
       name: "Keepr",
       description: "An application designed to share photos with one another. The app works similarly to Pinterest, you can add keeps(photos) to a vault. Logged in users can have private vaults that are inaccessible and not-viewable by non authorized accounts",
-      image: cwChallenges,
+      image: keepr,
       source_code_link: "https://github.com/codeworksacademy/challenges.codeworksacademy.com",
       source_code_icon: github,
       cssSelector: "cw-challenges",
@@ -38,7 +38,7 @@ const projectData = {
       id: "spuditions",
       name: "Spuditions",
       description: "Spuditions is a website built for those who love the outdoors. This application provides users with information about Idaho state parks. When using our website, you can see the weather, pricing, directions, and reviews for your desired location.",
-      image: "",
+      image: spuditions,
       source_code_link: "https://spuditions.com/",
       source_code_icon: github,
       cssSelector: "grant-ready",
@@ -64,7 +64,7 @@ const projectData = {
     {
       name: "Inspire",
       description: "Inspire is a sleek designed homepage for your web-browser. It is a simple application, displaying the date, time, and weather. A bonus of this application is the ability to add 'todos' that you can check off when complete. If you are feeling like life is to cluttered, use Inspire to help simplify your day",
-      image: threaditAI,
+      image: inspire,
       hyper_link: "https://threaditai.web.app",
       source_code_link: "https://github.com/AJVancattenburch/ThreadItAI",
       source_code_icon: github,
